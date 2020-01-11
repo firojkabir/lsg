f3a82b0b8293be101703cafa64fded7b63c5b7dd
FirozKabir65 <firoj.kabir007@gmail.com>
2020-01-11 11:44:20 +0100

    Working on login page

---
+++ resources/views/frontend/login.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.frontend_base')
+
+@section('content')
+	<div id="divImg">
+		<img src="{{ asset('static/website/themes/images/fulda2.jpg') }}" alt="" class="img img-responsive">
+	</div>
+	
+	<div id="mainBody">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
