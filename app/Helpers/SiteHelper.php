537dde59ab4db1b77b7a2c8b08c8633dfa8d4667
jobayermojumder <jobayer.pro@gmail.com>
2020-01-11 23:42:33 +0100

    Helpers modified

--- app/Helpers/SiteHelper.php
+++ app/Helpers/SiteHelper.php
@@ -5,23 +5,8 @@ use DB;
 
 class SiteHelper
 {
-	public static function awards(){
-		$results = DB::table('aboutus_awards')->where('hstatus', 1)->orderBy('date', 'DESC')->limit(6)->get();
-		return $results;
-	}
-
-	public static function chairmanMsg(){
-		$results = DB::table('people_chairmanmsg')->first();
-		return $results;
-	}
-
-	public static function mdMsg(){
-		$results = DB::table('people_mdmsg')->first();
-		return $results;
-	}
-
-	public static function serveProducts(){
-		$results = DB::table('home_serve')->where('status', 1)->orderBy('rank', 'ASC')->get();
+	public static function category(){
+		$results = DB::table('category')->where('status', 1)->orderBy('name', 'ASC')->get();
 		return $results;
 	}
 
