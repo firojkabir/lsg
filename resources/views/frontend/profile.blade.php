eaf1f48de4f3517de2e754741281745c80c08e10
FirozKabir65 <firoj.kabir007@gmail.com>
2020-01-13 23:27:22 +0100

    Profile page design done

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -25,17 +25,17 @@
                     </div><!-- /.sidebar -->
                 </div><!-- /.col-* -->
                 
-                <div class="span9">
+                <div class="profile span9">
                     <div class="row content">
                         <div class="page-title">
                             <h2>Profile
-                                <a class="btn btn-primary btn-xs pull-right" href="/edit_profile">Edit Profile</a>
+                                <a class="btn btn-info btn-xs pull-right" href="/edit_profile">Edit Profile</a>
                             </h2>
-                        </div><!-- /.page-title -->
+                        </div>
                         <hr>
                         <div class="background-white p20 mb10">
                             <h4 class="page-title">Contact Information</h4>
-                            <hr>
+                            <hr class="hr_exception">
 
                             <form class="contact-form">
                                 <div class="span4">
@@ -76,33 +76,54 @@
                                         </div>
                                     </div>
                                 </div>
+                                
+                                <hr class="hr_exception">
+                                <h4 class="page-title">Address</h4>
+                                <hr class="hr_exception">
+
+                                <div class="span4">
+                                    <div class="control-group">
+                                        <div class="controls">
+                                            <label>Street</label>
+                                            <p>
+                                                <label>{{ Auth::guard('client')->user()->street }}</label>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="control-group">
+                                        <div class="controls">
+                                            <label>City</label>
+                                            <p>
+                                                <label>{{ Auth::guard('client')->user()->city }}</label>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="span5">
+                                    <div class="control-group">
+                                        <div class="controls">
+                                            <label>ZIP</label>
+                                            <p>
+                                                <label>{{ Auth::guard('client')->user()->zip }}</label>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="control-group">
+                                        <div class="controls">
+                                            <label>Country</label>
+                                            <p>
+                                                <label>{{ Auth::guard('client')->user()->country }}</label>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </form>
-                            
-                        </div>
-                        <div class="background-white p20 mb10">
-                            <h3 class="page-title">Address</h3>
-                            <div class="row">
-                                <div class="form-group col-sm-6">
-                                    <label>Strret</label>
-                                    <p><label>{{ Auth::guard('client')->user()->street }}</label></p>
-                                </div><!-- /.form-group -->
-                                <div class="form-group col-sm-6">
-                                    <label>City</label>
-                                    <p><label>{{ Auth::guard('client')->user()->city }}</label></p>
-                                </div><!-- /.form-group -->
-                                <div class="form-group col-sm-6">
-                                    <label>ZIP</label>
-                                    <p><label>{{ Auth::guard('client')->user()->zip }}</label></p>
-                                </div><!-- /.form-group -->
-                                <div class="form-group col-sm-6">
-                                    <label>Country</label>
-                                    <p><label>{{ Auth::guard('client')->user()->country }}</label></p>
-                                </div><!-- /.form-group -->
-                            </div><!-- /.row -->
                         </div>
                     </div>
-                </div><!-- /.content -->
-            </div><!-- /.col-* -->
+                </div>
+            </div>
         </div>
     </section>
 
