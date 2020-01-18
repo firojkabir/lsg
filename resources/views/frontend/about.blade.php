9712cf833ae68ba88790a8bc75eaec766ecafb0c
FirozKabir65 <firoj.kabir007@gmail.com>
2020-01-18 11:47:59 +0100

    Linked about us page and add breadcrumb

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -4,4 +4,17 @@
 	<div id="divImg">
 		<img src="{{ asset('static/website/themes/images/fulda2.jpg') }}" alt="" class="img img-responsive">
 	</div>
+
+	<div id="mainBody">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<ul class="breadcrumb">
+						<li><a href="/">Home</a> <span class="divider">/</span></li>
+						<li class="active">About Us</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
