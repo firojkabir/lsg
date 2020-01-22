f0ed80c29996541b013be9d2dd06e1d8c5a51f7c
FirozKabir65 <firoj.kabir007@gmail.com>
2020-01-22 20:47:58 +0100

    table and btn modification on Cart summery page

--- resources/views/frontend/cart-summery.blade.php
+++ resources/views/frontend/cart-summery.blade.php
@@ -14,12 +14,12 @@
 				@include('frontend.include.sidebar')
 				<!-- ******* Sidebar end ******* -->
 
-				<div class="span9">
+				<div class="cart_summery span9">
 					<ul class="breadcrumb">
 						<li><a href="index.html">Home</a> <span class="divider">/</span></li>
 						<li class="active"> SHOPPING CART</li>
 					</ul>
-					<h3>  SHOPPING CART<a href="/products" class="btn btn-large pull-right"><i class="icon-arrow-left"></i> Continue Shopping </a></h3>
+					<h4>  SHOPPING CART<a href="/products" class="btn btn-large pull-right"><i class="icon-arrow-left"></i> Continue Shopping </a></h4>
 					<hr class="soft"/>
 					@php
 						$items = Cart::content();
@@ -31,9 +31,7 @@
 								<th>Action</th>
 								<th>Description</th>
 								<th>Quantity/Update</th>
-								<th>Price</th>{{-- 
-								<th>Discount</th>
-								<th>Tax</th> --}}
+								<th>Price</th>
 								<th>Total</th>
 							</tr>
 						</thead>
@@ -47,71 +45,20 @@
 								<td>&euro;{{ $row->price*$row->qty }}</td>
 							</tr>
 							@endforeach
-							<tr style="empty-cells: hide;">
+							<tr {{-- style="empty-cells: hide;" --}}>
 								<td></td>
 								<td></td>
 								<td></td>
-								<td></td>
+								<td><b>Subtotal:</b></td>
 								<td>&euro;{{ Cart::subtotal() }}</td>
 							</tr>
 						</tbody>
 					</table>
 					<button type="btn" class="btn btn-success pull-right">Confirm order</button>
-					
-					
-					{{-- <table class="table table-bordered">
-						<tbody>
-							<tr>
-								<td>
-									<form class="form-horizontal">
-										<div class="control-group">
-											<label class="control-label"><strong> VOUCHERS CODE: </strong> </label>
-											<div class="controls">
-												<input type="text" class="input-medium" placeholder="CODE">
-												<button type="submit" class="btn"> ADD </button>
-											</div>
-										</div>
-									</form>
-								</td>
-							</tr>
-							
-						</tbody>
-					</table>
-					
-					<table class="table table-bordered">
-						<tr><th>ESTIMATE YOUR SHIPPING </th></tr>
-						<tr>
-							<td>
-								<form class="form-horizontal">
-									<div class="control-group">
-										<label class="control-label" for="inputCountry">Country </label>
-										<div class="controls">
-											<input type="text" id="inputCountry" placeholder="Country">
-										</div>
-									</div>
-									<div class="control-group">
-										<label class="control-label" for="inputPost">Post Code/ Zipcode </label>
-										<div class="controls">
-											<input type="text" id="inputPost" placeholder="Postcode">
-										</div>
-									</div>
-									<div class="control-group">
-										<div class="controls">
-											<button type="submit" class="btn">ESTIMATE </button>
-										</div>
-									</div>
-								</form>
-							</td>
-						</tr>
-					</table>
-					<a href="products.html" class="btn btn-large"><i class="icon-arrow-left"></i> Continue Shopping </a>
-					<a href="login.html" class="btn btn-large pull-right">Next <i class="icon-arrow-right"></i></a> --}}
-					
 				</div>
-
-
-
 			</div>
 		</div>
 	</div>
+
+
 @endsection
