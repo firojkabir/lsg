0a212ec969f4c0d0cb9591f2af78c0e9fafe02f6
FirozKabir65 <firoj.kabir007@gmail.com>
2020-01-27 19:37:30 +0100

    edit_profile done

--- resources/views/frontend/edit_profile.blade.php
+++ resources/views/frontend/edit_profile.blade.php
@@ -7,18 +7,18 @@
     <br>
     
     <form action="" method="POST" enctype="multipart/form-data">
-        <section class="htc__product__grid bg__white ptb--50">
+        <section class="edit_profile htc__product__grid bg__white ptb--50">
             <div class="container">
                 <div class="row">
-                    <div class="span3">
+                    <div class="photo span3">
                         <div class="sidebar">
                             <div class="widget">
                                 <div class="user-photo">
                                     <div class="row">
                                         <div class="col-smx-2 imgUp">
                                             <div class="imagePreview"></div>
-                                            <label class="btn btn-primary">
-                                            Upload<input type="file" class="file" value="Upload Photo" style="width: 100%: 0px;overflow: hidden;" name="uploadedfile" id="image">
+                                            <label class="btn">
+                                            Upload Photo<input type="file" class="file" value="Upload Photo" style="width: 100%: 0px;overflow: hidden;" name="uploadedfile" id="image">
                                             </label>
                                         </div>
                                     </div>
@@ -38,48 +38,52 @@
                             </div>
                             <hr>
                             <div class="background-white p20 mb10">
-                                <h3 class="page-title">Contact Information</h3>
-                                <hr>
+                                <h4 class="page-title">Contact Information</h4>
+                                <hr class="hr_exception">
                                 <div class="row">
                                     <div class="form-group span4">
-                                        <label>Firstname</label>
+                                        <label><b>Firstname</b></label>
                                         <input type="text"  class="form-control" value="" name="firstname" required>
                                     </div>
-                                    <div class="form-group span5">
-                                        <label>Lastname</label>
+                                    <div class="form-group span4">
+                                        <label><b>Lastname</b></label>
                                         <input type="text"  class="form-control" value="" name="lastname" required>
                                     </div>
                                     <div class="form-group span4">
-                                        <label>E-mail</label>
+                                        <label><b>E-mail</b></label>
                                         <input type="text"  class="form-control" value="" name="email" required>
                                     </div>
-                                    <div class="form-group span5">
-                                        <label>Phone</label>
+                                    <div class="form-group span4">
+                                        <label><b>Phone</b></label>
                                         <input type="text"  class="form-control" value="" name="phone" required>
                                     </div>
                                 </div>
                             </div>
                             <div class="background-white p20 mb10">
-                                <h3 class="page-title">Address</h3>
+                                <hr class="hr_exception">
+                                <h4 class="page-title">Address</h4>
+                                <hr class="hr_exception">
                                 <div class="row">
                                     <div class="form-group span4">
-                                        <label>Street</label>
+                                        <label><b>Street</b></label>
                                         <input type="text"  class="form-control" value="" name="state" required>
                                     </div>
-                                    <div class="form-group span5">
-                                        <label>City</label>
+                                    <div class="form-group span4">
+                                        <label><b>City</b></label>
                                         <input type="text"  class="form-control" value="" name="city" required>
                                     </div>
                                     <div class="form-group span4">
-                                        <label>ZIP</label>
+                                        <label><b>ZIP</b></label>
                                         <input type="text"  class="form-control" value="" name="zip" required>
                                     </div>
-                                    <div class="form-group span5">
-                                        <label>Country</label>
+                                    <div class="form-group span4">
+                                        <label><b>Country</b></label>
                                         <input type="text"  class="form-control" value="" name="country" required>
                                     </div>
                                 </div>
                             </div>
+                            <br>
+                            <button type="button" class="btn btn-success pull-right">Update Info</button>
                         </div>
                     </div>
                 </div>
